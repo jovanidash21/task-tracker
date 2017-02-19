907bb66efc1847399936b2bf5dcb8f3848efdb82
jovanidash21 <jwardash21@gmail.com>
2017-02-19 22:43:18 +0800

    Add Login Page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', {
+    title: 'Login | Todo List'
+  });
 });
 
 module.exports = router;
