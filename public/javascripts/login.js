0d24982c929d7413f242ecee982c31250b2485be
jovanidash21 <jwardash21@gmail.com>
2017-02-22 10:15:02 +0800

    Change Login Page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,3 @@
-$('.toggle').on('click', function() {
-    $('.container').stop().addClass('active');
-});
-
-$('.close').on('click', function() {
-    $('.container').stop().removeClass('active');
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
