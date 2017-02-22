1877a888d4426638a1a685fe7d2bab81b8878ea0
jovanidash21 <jwardash21@gmail.com>
2017-02-23 00:03:01 +0800

    Add Loading Animation Component

---
+++ src/components/LoadingAnimation/Index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+var Spinner = require('react-spinkit');
+
+const LoadingAnimation  = () => {
+    return(
+        <section className="loading-animation">
+            <Spinner spinnerName='wandering-cubes' />
+        </section>
+    )
+};
+
+export default LoadingAnimation;
