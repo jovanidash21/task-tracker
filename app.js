1067abb98ea0f651f418ecc9aa8b6da6d490c751
jovanidash21 <jwardash21@gmail.com>
2017-02-24 16:39:20 +0800

    Add favicon.ico

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ mongoose.connect(process.env.MONGODB_URI, options);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
