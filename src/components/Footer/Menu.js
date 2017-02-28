0aeaf0e14598191585f4ebaebf2ff5942779471a
jovanidash21 <jwardash21@gmail.com>
2017-02-28 22:58:52 +0800

    Update Footer

--- src/components/Footer/Menu.js
+++ src/components/Footer/Menu.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 
-class Menu extends Component {
-    render() {
-        return(
-            <div className="copyright">
-                <ul className="menu">
-                    <li>
-                        &copy; 2017. All rights reserved.
-                    </li>
-                    <li>
-                        <a href="#" target="_blank">
-                            GitHub Project
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const Menu = () => {
+    return(
+        <div className="copyright">
+            <ul className="menu">
+                <li>
+                    &copy; 2017. All rights reserved.
+                </li>
+                <li>
+                    <a href="#" target="_blank">
+                        GitHub Project
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+};
 
 export default Menu;
