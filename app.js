be25fbb25f1d44ed6f2237a5ec069a58bf81c270
jovanidash21 <jwardash21@gmail.com>
2017-04-04 20:53:34 +0800

    Convert Jade To Pug

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ mongoose.Promise = Promise;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
